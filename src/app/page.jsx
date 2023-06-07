504b50f78f13a01cb6b08eef2675d800058bfde1
Guapura89 <betocrack1228@gmail.com>
2023-06-06 19:08:12 -0600

    test: testing calendar dependency

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
+"use client";
 import Image from "next/image";
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function Home() {
+  const [value] = useState(new Date());
+
   return (
-    <main className="">
-      <div className="">
-        <h1>AnimalZone app</h1>
+    <main className="h-screen flex flex-col items-center justify-center">
+      <h1>AnimalZone app</h1>
+      <div className="w-1/2 flex justify-center">
+        <Calendar value={value} />
       </div>
     </main>
   );
