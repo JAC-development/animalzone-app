c5f878ca944b64680eb6bf8700f022792efd3166
Guapura89 <betocrack1228@gmail.com>
2023-06-07 19:27:48 -0600

    AZA-12 feat: add color palette :art:

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col items-center justify-center">
+    <main className="h-screen flex flex-col items-center bg-informative-p justify-center">
       <h1>AnimalZone app</h1>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary-y': '#F5CB5B',
+        'primary-g': '#E7FFAC',
+        dark: '#333532',
+        'secondary-g': '#EFFFC6',
+        'secondary-gy': '#A9A9A9',
+        'secondary-dy': '#BD9E4A',
+        'informative-g': '#D0E8D6',
+        'informative-p': '#FFA0A0',
+        'informative-y': '#FFF9C5',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme('fontSize.3xl') },
+        h2: { fontSize: theme('fontSize.2xl') },
+        h3: { fontSize: theme('fontSize.lg') },
+      });
+    }),
+  ],
 };
