99d6205a5c34850c5f4ce6775a83ee6f2c58c21c
Guapura89 <betocrack1228@gmail.com>
2023-06-10 02:21:25 -0600

    add domain to lorem images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['picsum.photos', 'fastly.picsum.photos'],
+  },
 };
 
 module.exports = nextConfig;
