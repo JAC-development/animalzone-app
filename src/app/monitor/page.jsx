d154077f23ff06c24ff890f549c1807d64a8bdab
Angel Lopez <srterradev@gmail.com>
2023-07-18 14:12:02 -0600

    AZA-23 auto width fixed

--- src/app/monitor/page.jsx
+++ src/app/monitor/page.jsx
@@ -13,7 +13,7 @@ export default function MonitorView() {
   // }, []);
 
   return (
-    <div className="px-8 py-12 lg:px-14 xl:px-24">
+    <div className="px-8 py-12 lg:px-14 xl:px-24 lg:pt-24 w-full">
       {/* Go back section on top */}
       <div className="flex gap-4">
         <div className="flex items-start">
