7d079213e9eb7c77fa2c3982d11ccef6a25a9727
Guapura89 <betocrack1228@gmail.com>
2023-07-23 15:45:56 -0600

    fix: fix layout mount bug :bug:

--- src/app/monitor/layout.jsx
+++ src/app/monitor/layout.jsx
@@ -2,14 +2,10 @@ import { Menu, NavMobiile } from '@components/Menu';
 
 export default function monitorLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <main className="lg:flex">
-          <Menu />
-          <NavMobiile />
-          {children}
-        </main>
-      </body>
-    </html>
+    <main className="lg:flex">
+      <Menu />
+      <NavMobiile />
+      {children}
+    </main>
   );
 }
--- src/app/user/layout.jsx
+++ src/app/user/layout.jsx
@@ -2,14 +2,10 @@ import { Menu, NavMobiile } from '@components/Menu';
 
 export default function userLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <main className="lg:flex">
-          <Menu />
-          <NavMobiile />
-          {children}
-        </main>
-      </body>
-    </html>
+    <main className="lg:flex">
+      <Menu />
+      <NavMobiile />
+      {children}
+    </main>
   );
 }
