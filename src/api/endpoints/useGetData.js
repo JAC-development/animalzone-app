c7d98a60751f96b464e57168f017212b7a321f0e
Guapura89 <betocrack1228@gmail.com>
2023-08-24 17:07:22 -0600

    AZA-30 feat: add register assistance on scann sparkles
    - Save on database
    
    - Check range cooords

--- src/api/endpoints/useGetData.js
+++ src/api/endpoints/useGetData.js
@@ -1,5 +1,5 @@
 'use client';
-import { getFirestore, collection, query, getDocs, where, addDoc } from 'firebase/firestore';
+import { getFirestore, collection, query, getDocs, where, addDoc, setDoc, doc, getDoc } from 'firebase/firestore';
 import { app } from 'firebaseConfig';
 import bcrypt from 'bcryptjs';
 
@@ -43,6 +43,68 @@ export async function handleAddData(data) {
   }
 }
 
+export async function handleGetUser(user) {
+  console.log(user, 'Este llegoooo');
+  const snapshot = await getDocs(collection(firestore, 'attendance', user, 'assistance'));
+
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  return data;
+}
+
+export async function userExists(user) {
+  console.log(user, 'Este llegoooo');
+  const snapshot = await getDoc(doc(firestore, 'attendance', user));
+  console.log(snapshot, 'exist');
+
+  if (snapshot.exists()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export async function userStatus(user) {
+  const snapshot = await getDoc(doc(firestore, 'attendance', user));
+
+  const data = snapshot.data();
+  return data.status;
+}
+
+export async function handleAddAttendance(data) {
+  const exist = userExists(data.uid);
+  if ((await exist) === true) {
+    const status = await userStatus(data.uid);
+    addDoc(collection(firestore, 'attendance', data.uid, 'history'), {
+      uid: data.uid,
+      user: data.user,
+      inPlace: data.inPlace,
+      date: data.date,
+      status: status === 'entrada' ? 'salida' : 'entrada',
+    })
+      .then(async (res) => {
+        console.log(res.id);
+        console.log('status:', status);
+        setDoc(doc(firestore, 'attendance', data.uid), { status: status === 'entrada' ? 'salida' : 'entrada' });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    setDoc(doc(firestore, 'attendance', data.uid), { status: 'entrada' });
+    addDoc(collection(firestore, 'attendance', data.uid, 'history'), {
+      uid: data.uid,
+      user: data.user,
+      inPlace: data.inPlace,
+      date: data.date,
+      status: 'entrada',
+    });
+  }
+}
+
 export async function handleGetAllData() {
   const snapshot = await getDocs(collection(firestore, 'usuarios'));
 
@@ -53,14 +115,3 @@ export async function handleGetAllData() {
 
   return data;
 }
-
-// export async function handleGetAllData() {
-//   const snapshot = await getDocs(query(collection(firestore, 'usuarios'), where('email', '!=', '')));
-
-//   const data = snapshot.docs.map((doc) => ({
-//     id: doc.id,
-//     ...doc.data(),
-//   }));
-
-//   return data;
-// }
