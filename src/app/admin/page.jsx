f435f0f12a6dd0a2098f1f1d360203639966c339
Guapura89 <betocrack1228@gmail.com>
2023-08-28 13:52:38 -0600

    move notify function to Admin layout

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,66 +1,14 @@
 'use client';
-import { getFirestore, collection, query, onSnapshot } from 'firebase/firestore';
-import { app } from 'firebaseConfig';
 import { ChevronRightIcon, EllipsisHorizontalCircleIcon } from '@heroicons/react/24/solid';
 import { useState, useContext } from 'react';
 import { AuthContext } from 'hooks/useAuth';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { handleIdToName } from 'api/endpoints/useGetData';
-
-// get the firestore
-const firestore = getFirestore(app);
 
 export default function Admin() {
   const show = true;
   const [openTab, setOpenTab] = useState('viewUsers');
   const { userData } = useContext(AuthContext);
-  const id = 'animalzone';
 
-  const notifyIn = (text) => {
-    toast.success(text, {
-      position: 'bottom-right',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: false,
-      draggable: true,
-      toastId: id,
-      icon: false,
-      progress: undefined,
-      theme: 'colored',
-    });
-    toast.clearWaitingQueue();
-  };
-  const notifyOut = (text) => {
-    toast.error(text, {
-      position: 'bottom-right',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: false,
-      draggable: true,
-      toastId: id,
-      icon: false,
-      progress: undefined,
-      theme: 'colored',
-    });
-    toast.clearWaitingQueue();
-  };
-  const q = query(collection(firestore, 'attendance'));
-  // onSnapshot(q, (snapshot) => {
-  //   snapshot.docChanges().forEach(async (change) => {
-  //     if (change.type === 'modified') {
-  //       const name = await handleIdToName(change.doc.id);
-  //       console.log(change.doc.data().status);
-  //       if (change.doc.data().status === 'entrada') {
-  //         notifyIn(`${name} ha llegado`);
-  //       } else {
-  //         notifyOut(`${name} ha salido`);
-  //       }
-  //     }
-  //   });
-  // });
   return (
     <div className="px-8 pb-16 pt-24 lg:px-14 xl:px-24 flex flex-col justify-between">
       {/* Main section */}
@@ -185,7 +133,6 @@ export default function Admin() {
           </div>
         </div>
       </div>
-      <ToastContainer limit={1} />
     </div>
   );
 }
